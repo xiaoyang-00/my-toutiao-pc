a0d0d7df34685304fbf520f6182a8b82bf401183
xiaoyang <1764974647@qq.com>
2020-07-21 22:03:00 +0800

    登录模块-安装axios库

--- src/main.js
+++ src/main.js
@@ -9,6 +9,12 @@ Vue.use(ElementUi)
 // 导入路由模块
 import router from '@/router'
 
+// 导入axios库
+import axios from 'axios'
+// 挂载->将来在任何组件访问$http属性就是访问axios进行请求
+Vue.prototype.$http = axios
+
+
 Vue.config.productionTip = false
 
 // 挂载到根实例
